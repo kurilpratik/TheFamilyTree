3e219714fe4274293158692aa382e4a7991587cb
Pratik Kuril <pratikkuril16@gmail.com>
2025-05-12 07:44:39 +0530

    Organize nav data under data

---
+++ src/data/NavData.js
@@ -0,0 +1,54 @@
+const IndexNavData = [
+  {
+    title: 'Home',
+    link: '#home',
+  },
+  {
+    title: 'Team',
+    link: '#team',
+  },
+  {
+    title: 'Publications',
+    link: '#prints',
+  },
+  {
+    title: 'Seminars',
+    link: '#seminars',
+  },
+  {
+    title: 'Contact',
+    link: '#appointments',
+  },
+  {
+    title: 'Emotion Centre',
+    link: 'emotion.html',
+  },
+];
+
+const AdultsNavData = [
+  {
+    title: 'Home',
+    link: '#home',
+  },
+  {
+    title: 'Contact',
+    link: '#appointments',
+  },
+  {
+    title: 'Emotion Centre',
+    link: 'emotion.html',
+  },
+];
+
+const EmotionNavData = [
+  {
+    title: '< Family Tree',
+    link: '/index.html',
+  },
+  {
+    title: 'Contact',
+    link: '#contact',
+  },
+];
+
+export { IndexNavData, AdultsNavData, EmotionNavData };
