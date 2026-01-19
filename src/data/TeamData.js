54fc5e0bdcd039d3145ccf15a8d3269d011b64ae
Pratik Kuril <pratikkuril16@gmail.com>
2026-01-20 04:00:20 +0530

    Update the team data

--- src/data/TeamData.js
+++ src/data/TeamData.js
@@ -5,57 +5,60 @@ const TeamData = [
     shortText:
       'Nupur D. Paiva is a Chartered Clinical Psychologist, Associate Fellow with the British Psychological Society, Child Psychotherapist and mother-of-two, all of which she has worked very hard at. Especially, the two children.',
     fullText: `Nupur D. Paiva is a Chartered Clinical Psychologist, Associate Fellow with the British Psychological Society, Child Psychotherapist and mother-of-two, all of which she has  worked very hard at. Especially, the two children.
-        <br />
+        <br /><br />
         She started Family Tree: Child and Adolescent Mental Health team, in 2018 in New Delhi. She taught 'Infant Observation' on the Psychotherapy training course at Ambedkar University, Delhi from 2012-2020. She is regularly invited to write articles on emotional and mental health for national dailies and for discussions on conference panels. You can find a lot of this through a google search.
-        <br />
+        <br /><br />
         In 2014 she decided to train further, especially on how to work with hard-to-engage-adolescents, anxiety and depression in young people. She completed a certification in Intensive Short Term Dynamic Psychotherapy. The process of this training took five years of consistent truth-facing and changed her life, making her more courageous. Now she is training others to work with this technique. She is the author of Love & Rage: The Inner Worlds of Children, which was published in 2017, which is very close to her heart and has a lot of detail on what psychotherapy with children looks like. Along with her husband, Richard Paiva, she is a co-founder of The Art of Sport – a development programme for girls, using sport and group therapy.
-        <br />
+        <br /><br />
         For her own energy and emotional balance, she likes to dance and though her motor coordination does not approve, she does not let that stop her.`,
   },
   {
     name: 'Neha Gupta',
-    image: '/assets/Team/neha.jpg',
+    image: '/assets/Team/neha.png',
     shortText:
-      'The first to join in 2018 was Neha Gupta when Family Tree was a wee tentative experiment. It was with her that we became a team. She and I. Super well-trained, Neha has an MSc Child & Adolescent Mental Health and Psychological Practice from The University of Edinburgh, UK and a BA in Psychology (Honours) from Lady Shri Ram College, Delhi University.',
-    fullText: `The first to join in 2018 was Neha Gupta when Family Tree was a wee tentative experiment. It was with her that we became a team. She and I. Super well-trained, Neha has an MSc Child & Adolescent Mental Health and Psychological Practice from The University of Edinburgh, UK and a BA in Psychology (Honours) from Lady Shri Ram College, Delhi University.
-        <br />
-        More than the fact that she is psychoanalytically trained at PTRC (Psychoanalytic Training and Research Centre), Mumbai, it is her natural empathy toward the emotional world of young humans that makes her an effective child psychotherapist.
-        <br />
-        She has worked with children and young adults for over 7 years in a variety of settings. She has also worked with children with special developmental needs.
-        <br />
-        Neha facilitates a group of girls between the ages 10-13 years at @artofsportindia too.
-        <br />
-        Neha's first true love has always been animals. She's a dog lover and is happy to spend the rest of her life being a Dog mom.`,
+      'The first to join in 2018 was Neha Gupta when Family Tree was a wee tentative experiment. It was with her that we became a team. She and I.',
+    fullText: `The first to join in 2018 was Neha Gupta when Family Tree was a wee tentative experiment. It was with her that we became a team. She and I.
+    <br /><br />
+    Neha is a highly trained psychotherapist, holding an MSc in Child & Adolescent Mental Health and Psychological Practice from the University of Edinburgh, UK, and a BA (Honours) in Psychology from Lady Shri Ram College, Delhi University. She is psychoanalytically trained at the Psychoanalytic Training and Research Centre (PTRC), Mumbai. Beyond her formal training, it is her deep, intuitive empathy for the emotional worlds of human beings that lies at the heart of her therapeutic work.
+    <br /><br />
+    She has over 12 years of experience working with children and young adults across diverse settings, including work with children with special developmental needs. Trained in Intensive Short-Term Dynamic Psychotherapy (ISTDP), Neha has also been working with adults for over six years.
+    <br /><br />
+    Having recently relocated to Goa, Neha now runs the Family Tree branch based in Panjim.
+    Outside the therapy room, Neha’s first and enduring love has been animals and she happily identifies as a lifelong dog mom. In recent years, she has discovered a passion for movement, spending much of her free time exploring the limits of her body and deepening the connection between mind and muscle.`,
   },
   {
     name: 'Ambika Singh',
     image: '/assets/Team/ambika.jpg',
     shortText:
-      'Ambika joined Family Tree in February 2020 and has valiantly worked online with adolescents throughout this lockdown. Ambika Singh is a trained Psychoanalytic Psychotherapist having completed her MPhil from Ambedkar University Delhi; Masters in Psychosocial Clinical Studies from Ambedkar University, Delhi; and a Bachelors in Psychology (Honours) from Lady Shri Ram College, Delhi University.',
-    fullText: `Ambika joined Family Tree in February 2020 and has valiantly worked online with adolescents throughout this lockdown. Ambika Singh is a trained Psychoanalytic Psychotherapist having completed her MPhil from Ambedkar University Delhi; Masters in Psychosocial Clinical Studies from Ambedkar University, Delhi; and a Bachelors in Psychology (Honours) from Lady Shri Ram College, Delhi University.
-        <br />
-        She has worked in places few dare to tread - mental health centres in university settings. She has borne the red-tape, the senior administration who don't understand the emotional issues of young people, the high rates of self harm and suicidal thoughts in her clients.
-        <br />
-        At Family Tree we are grateful to Ambika for her experience and commitment to working on the darkest of real subjects - understanding 'suicidality' and its representations in the Indian context.
-        <br />
-        She is occasionally frightened of working with small kids though - and we are helping her get over that.
-        <br />
-        When she isn't working, she can be found dancing to 90s music or talking to her plants.`,
+      'Ambika Singh is a trained Psychoanalytic Psychotherapist having completed her MPhil from Ambedkar University Delhi; Masters in Psychosocial Clinical Studies from Ambedkar University, Delhi; and a Bachelors in Psychology (Honours) from Lady Shri Ram College, Delhi University.',
+    fullText: `Ambika Singh is a trained Psychoanalytic Psychotherapist having completed her MPhil from Ambedkar University Delhi; Masters in Psychosocial Clinical Studies from Ambedkar University, Delhi; and a Bachelors in Psychology (Honours) from Lady Shri Ram College, Delhi University.
+    <br /><br />
+    She joined Family Tree in February 2020 and valiantly worked online with adolescents throughout the lockdown.
+    <br /><br />
+    Ambika has worked in places few dare to tread - mental health centres in university settings. She has borne the red-tape, the senior administration who don't understand the emotional issues of young people, the high rates of self harm and suicidal thoughts in her clients.
+    <br /><br />
+    At Family Tree we are grateful to Ambika for her experience and commitment to working on the darkest of real subjects - understanding 'suicidality' and its representations in the Indian context.
+    <br /><br />
+    She is the author of Interruptions in Identity: Engaging with Suicidality among the Indian Youth, a Sage Yoda Press Publication, August 2021. Framed within the context of the alarming rise of suicide rates in India, her work as a psychotherapist and researcher reflects on the interpersonal nature inherent in feelings of suicidality.
+    <br /><br />
+    In 2025, she completed the core training in Intensive Short Term Dynamic Psychotherapy making her an ISTDP Practitioner, certified by the ISTDP Institute, Washington D.C.
+    <br /><br />
+    When she isn't working, she can be found dancing to 90s music or talking to her plants.`,
   },
   {
     name: 'Shweta Dharamdasani',
     image: '/assets/Team/shweta.jpg',
     shortText:
-      'Shweta Dharamdasani is a psychoanalytically oriented psychotherapist with an MPhil from Ambedkar University, Delhi. She is a researcher and writer, committed to depth thinking and learning. Her area of interest is in how culture and psyche influence each other.',
-    fullText: `Shweta Dharamdasani is a psychoanalytically oriented psychotherapist with an MPhil from Ambedkar University, Delhi. She is a researcher and writer, committed to depth thinking and learning. Her area of interest is in how culture and psyche influence each other.
-        <br />
-        Shweta has also been trained at Antarnaad foundation, Ahmadabad for a year and is a RCI certified Rehabilitation Psychologist, all of which makes her a highly prized clinician.
-        <br />
-        Shweta finds it very easy to connect with children and happily joins in the fitness group activities while she facilitates a group at @artofsportindia which she is missing terribly during the lockdown even though it is now online.
-        <br />
-        Recently she has also been working towards building a vocabulary for doing psychoanalysis in Hindi - an attempt to take psychoanalysis beyond the clinic and the elite, thereby making it more accessible.
-        <br />
-        Much like the children she works with through play and stories, Shweta enjoys craft activities-making things out of waste material and scrap. What we appreciate the most though is what she makes out of the distress, anxiety and silence that she works with in her young clients.`,
+      'Shweta Dharamdasani who joined us in 2019,  is a psychoanalytic psychotherapist with an MPhil from Ambedkar University, Delhi. She is a researcher and writer, committed to depth thinking and learning, with an interest in how culture and psyche influence each other.',
+    fullText: `Shweta Dharamdasani who joined us in 2019,  is a psychoanalytic psychotherapist with an MPhil from Ambedkar University, Delhi. She is a researcher and writer, committed to depth thinking and learning, with an interest in how culture and psyche influence each other.
+    <br /><br />
+    In addition, Shweta has trained at Antarnaad foundation, Ahmadabad and is a RCI certified Rehabilitation Psychologist, all of which makes her a highly prized clinician for Family Tree.
+    <br /><br />
+    She has been working towards building a vocabulary for doing psychotherapy in Hindi, in an attempt to take psychological work beyond the clinic and the elite, to make it more accessible. She has presented papers in International and National conferences and also published papers in reputed journals and books.
+    <br /><br />
+    In 2025, she completed the core training in Intensive Short Term Dynamic Psychotherapy making her an ISTDP Practitioner, certified by the ISTDP Institute, Washington D.C
+    <br /><br />
+    Much like the children she works with through play and stories, Shweta enjoys craft activities-making things out of wasted and scrap. What we appreciate the most though is what she makes out of the distress, anxiety and silence that she works with in her young clients.`,
   },
 ];
 
